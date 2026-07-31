0e3704e2b00935676c764a7bcb77a8810f20845b
ai-developer <ai-developer@rhoai3-coding-demo.local>
2026-07-31 15:09:53 +0000

    T-006: Harvest MapStruct mappers with Jakarta updates (mechanical verify-and-commit; O-T6)

---
+++ src/main/java/com/demo/mapper/SpecialtyMapper.java
@@ -0,0 +1,22 @@
+package com.demo.mapper;
+
+import org.mapstruct.Mapper;
+import com.demo.dto.SpecialtyDto;
+import com.demo.model.Specialty;
+
+import java.util.Collection;
+
+/**
+ * Map Specialty & SpecialtyDto using mapstruct
+ */
+@Mapper
+public interface SpecialtyMapper {
+    Specialty toSpecialty(SpecialtyDto specialtyDto);
+
+    SpecialtyDto toSpecialtyDto(Specialty specialty);
+
+    Collection<SpecialtyDto> toSpecialtyDtos(Collection<Specialty> specialties);
+
+    Collection<Specialty> toSpecialtys(Collection<SpecialtyDto> specialties);
+
+}
---
+++ src/main/java/com/demo/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.demo.mapper;
+
+import org.mapstruct.Mapper;
+import com.demo.dto.RoleDto;
+import com.demo.dto.UserDto;
+import com.demo.model.Role;
+import com.demo.model.User;
+
+import java.util.Collection;
+
+/**
+ * Map User/Role & UserDto/RoleDto using mapstruct
+ */
+@Mapper
+public interface UserMapper {
+    Role toRole(RoleDto roleDto);
+
+    RoleDto toRoleDto(Role role);
+
+    Collection<RoleDto> toRoleDtos(Collection<Role> roles);
+
+    User toUser(UserDto userDto);
+
+    UserDto toUserDto(User user);
+
+    Collection<Role> toRoles(Collection<RoleDto> roleDtos);
+
+}
